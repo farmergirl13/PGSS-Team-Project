64a622c75bd9defea75c943fa3f3c5ab166e2827
Elizabeth Farmer <Elizabeth Farmer>
2017-07-24 15:58:10 -0400

    Trying to update my code

--- teamProject.py
+++ teamProject.py
@@ -2,9 +2,8 @@ import timeit
 
 start = timeit.default_timer()
 
-import re
-s1 = "ACTCTCATGCTACLAKDASDFLSJFLJELIZABETHADJFLAJDFLK"
-s2 = "AECATGTCAAASJDFJSLDFJELIZABETHALKSDJFLJDFLJADS"
+s1 = "ACTCCAGTCATC"
+s2 = "AGCATGGGGTCA"
 
 start = timeit.default_timer()
 
@@ -18,19 +17,31 @@ def longest_substring(s1, s2):
                 t[x][y] = t[x-1][y-1] + 1
                 if t[x][y]>l:
                     l = t[x][y]
-                    xl  = x
-                    startPosition = x-l
+                    xl = x
+                    yl = y
             else:
                 t[x][y] = 0
-    addition = s1[xl-l: xl], (xl-l)
-    positionAndLength.append(addition)
-    return positionAndLength
-   
-print(longest_substring(s1, s2))
+    #returns substring and indexes in the first and second strings
+    return s1[xl-l: xl], (xl-l), (yl-l)
+    
 
-def removeLongestSubstring(string, substring, position):
-    print("It's me!!!")
-    print("Testing Git")
+def substringRevision(s1, s2):
+    count = 0
+    while(len(s1) != count):
+        #runs longest substring and returns substring and position in tuple
+        substringTuple = longest_substring(s1, s2)
+        print("SubstringTuple = ", substringTuple)
+        #replaces string 1 with a variable for the common substring
+        s1 = s1[0:substringTuple[1]] + str(count) + s1[(substringTuple[1] + len(substringTuple[0])):len(s1)]
+        
+        print("s1 = ", s1)
+        #replaces string 2 with a variable for the common substring
+        s2 = s2[0:substringTuple[2]] + str(count) + s2[(substringTuple[2] + len(substringTuple[0])):len(s2)]
+        print("s2 = ", s2)
+        count+=1
+        
+substringRevision(s1, s2)
+
     
 stop = timeit.default_timer()
 print("Run time: ", stop - start)
